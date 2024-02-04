f48599332e4a76f7cb290520339d7db390fb29f0
Tolik <t1005828@gmail.com>
2024-02-04 17:21:59 +0300

    Initial commit

---
+++ app/src/main/java/jolchu/tolik/me/a2d_flygame/Bullet.java
@@ -0,0 +1,36 @@
+package jolchu.tolik.me.a2d_flygame;
+
+import static jolchu.tolik.me.a2d_flygame.GameView.screenRatioX;
+import static jolchu.tolik.me.a2d_flygame.GameView.screenRatioY;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
+
+public class Bullet {
+
+    int x, y, width, height;
+    Bitmap bullet;
+
+    Bullet(Resources res) {
+
+        bullet = BitmapFactory.decodeResource(res, R.drawable.bullet);
+
+        width = bullet.getWidth();
+        height = bullet.getHeight();
+
+        width /= 4;
+        height /= 4;
+
+        width = (int) (width * screenRatioX);
+        height = (int) (height * screenRatioY);
+
+        bullet = Bitmap.createScaledBitmap(bullet, width, height, false);
+    }
+
+    Rect getCollisionShape() {
+        return new Rect(x, y, x + width, y + height);
+    }
+
+}
